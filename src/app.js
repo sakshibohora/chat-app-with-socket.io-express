7b9c46043bff892e9b91322a7299d6a7b803bcc1
Sakshi Bohora <sakshi.bohora@bacancytechnology.com>
2019-10-30 15:20:21 +0530

    broadcast message to each client

--- src/app.js
+++ src/app.js
@@ -6,21 +6,27 @@ app.get('/', (req, res) => {
 	res.sendfile('index.html');
 });
 
+let client = 0;
 //whenver someone connects
 io.on('connection', (socket) => {
 	console.log('A user connected');
-	setTimeout(()=>{
-		// socket.send('Message after 4 seconds')
+	// setTimeout(()=>{
+	// 	// socket.send('Message after 4 seconds')
 
-		  //Sending an object when emmiting an even
-		  socket.emit('testEvent',{description:'My new message'})
-	},4000)
+	// 	  //Sending an object when emmiting an even
+	// 	  socket.emit('testEvent',{description:'My new message'})
+	// },4000)
 
 	// handle event come from client side
 	// socket.on('clientEvent', function (data) {
 	// 	console.log(data);
 	// });
+	client++;
+	io.sockets.emit('broadcast', {
+		description: client + ' clients connected!'
+	});
 	socket.on('disconnect', () => {
+		client--;
 		console.log(' A user disconnected');
 	});
 });
